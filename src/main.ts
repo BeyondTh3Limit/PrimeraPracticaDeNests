52f43019cc872d860607c3aca647bf8c00a2f2f2
Abdiel Garcia <abdiel.garcia86u@std.uni.edu.ni>
2025-10-11 13:01:45 -0600

    Tercera guía completada

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'; // Note: DocumentBuilder is inferred from its usage
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
-async function bootstrap(): Promise<void> {
+async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
@@ -10,6 +10,15 @@ async function bootstrap(): Promise<void> {
     .setDescription('API description')
     .setVersion('1.0')
     .addTag('api')
+    .addBearerAuth({
+      type: 'http',
+      scheme: 'bearer',
+      bearerFormat: 'JWT',
+      in: 'header',
+      name: 'Authorization',
+      description: 'Enter your bearer token',
+    })
+    .addSecurityRequirements('bearer')
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
